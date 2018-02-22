e8b05aae7e97958c289774b0de3605807bff784b
Kyle Serrecchia <kyserrecchia@gmail.com>
2018-02-22 10:54:32 -0800

    added server, changed file names

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static('public'));
+
+
+app.get('/', function (req, res) {
+  res.sendFile('/index.html');
+});
+
+app.listen(process.env.PORT || 8080, function(){
+	console.log("running on 8080!");
+});
